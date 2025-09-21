6581dbddc6ee699d1e990e43c90c216e654017ef
Fedebap <tigrus245@gmail.com>
2025-09-21 19:02:36 -0300

    adaptadas variables locales a camelCase :)

--- src/Library/ICombatant.cs
+++ src/Library/ICombatant.cs
@@ -1,3 +1,4 @@
+namespace Library;
 // Defines the basic contract for any character that can participate in combat.
 // Every combatant has life points, can attack, defend, take damage, heal, and heal others.
 public interface ICombatant
@@ -8,5 +9,5 @@ public interface ICombatant
     void TakeDamage(double damage);
     void Attack(ICombatant target);
     void HealOthers(ICombatant target);
-    void TakeHeal(double HP);
+    void TakeHeal(double hp);
 }
