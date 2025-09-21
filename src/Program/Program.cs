109f755d28b43c8453435dbe1460083778bcb567
Pau <pauxxann@gmail.com>
2025-09-21 19:45:05 -0300

    item faltante anadido

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,9 +6,12 @@ class Program
     {
         Wizard yanfei = new Wizard("Yanfei", 150, 150);
         Wizard qiqi = new Wizard("Qiqi", 100, 100);
-        
-        Item espada = new Item("Espada", 30, 10, 0);
+    
+        Item espada = new Item("Rey de los Mares", 30, 10, 0);
         qiqi.AddItem(espada);
+        
+        Item grimorioReal = new Item("Grimorio Real", 20, 0, 15);
+        yanfei.AddItem(grimorioReal);
 
         Console.WriteLine($"Vida de Yanfei antes: {yanfei.Life}");
 
@@ -17,5 +20,5 @@ class Program
 
         yanfei.Heal();           
         Console.WriteLine($"Vida de Yanfei después de curar: {yanfei.Life}");
-    }   
+    }
 }
